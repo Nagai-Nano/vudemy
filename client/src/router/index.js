f9a22968388fffcb99cdd5f10f41aeade21d1a45
Nano <hikarishido1997@gmail.com>
2019-03-26 02:29:38 +0700

    config scroll behavior

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,7 +7,14 @@ import routes from './routes'
 
 const router = new Router({
   mode: 'history',
-  routes
+  routes,
+  // eslint-disable-next-line
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 })
 
 export default router
