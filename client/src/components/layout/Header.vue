d36e646e9264c2d308286a7c29000b88b348e752
Nano <hikarishido1997@gmail.com>
2019-03-25 20:30:24 +0700

    vue config

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -5,7 +5,20 @@
       <v-toolbar-title class="text-uppercase letter-spacing ma-0">
         <router-link to="/" class="white--text decoration-none">Vudemy</router-link>
       </v-toolbar-title>
+
       <v-spacer />
+      <v-text-field
+        placeholder="Search course..."
+        autofocus
+        solo
+        class="mt-2"
+        color="white"
+        flat
+        append-icon="search"
+        clearable
+      />
+      <v-spacer />
+
       <v-toolbar-items>
         <v-btn flat>Home</v-btn>
         <v-menu :nudge-width="100">
@@ -17,11 +30,14 @@
           </template>
 
           <template v-if="category.length">
-            <v-list class="py-0">
+            <v-list class="py-0 grey darken-4">
               <v-list-tile v-for="cat in category" :key="cat.id">
                 <v-list-tile-title>
-                  <router-link to="/" class="white--text decoration-none">
-                    {{ cat.title }}
+                  <router-link
+                    :to="`/courses/?category_id=${cat.id}`"
+                    class="white--text decoration-none"
+                  >
+                    <p class="pa-0 ma-0">{{ cat.title }}</p>
                   </router-link>
                 </v-list-tile-title>
               </v-list-tile>
@@ -41,10 +57,9 @@ export default {
     }
   },
   async created() {
-    const response = await fetch('http://localhost:1337/api/category')
+    const response = await fetch('/api/category')
     const json = await response.json()
     this.category = json.categories
-    console.log(this.category)
   }
 }
 </script>
@@ -53,5 +68,10 @@ export default {
 .v-menu__content {
   top: 64px !important;
   left: 1123px !important;
+}
+
+div[role='listitem']:hover {
+  cursor: pointer;
+  background: #424242;
 }
 </style>
