282e0faa57c157418d73469859856643f31da6b5
Nano <hikarishido1997@gmail.com>
2019-03-25 15:07:56 +0700

    api: get courses

---
+++ controllers/courses.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+exports.getCourses = async (req, res, next) => {
+  const { page = 1, size = 12, search, id } = req.query
+  let endpoint = `https://www.udemy.com/api-2.0/users/me/subscribed-courses?fields[course]=@min,visible_instructors,image_480x270&fields[user]=@min,job_title&ordering=-enroll_time&page=${page}&page_size=${size}`
+
+  if (search) {
+    endpoint = `${endpoint}&search=${search}`
+  }
+
+  if (id) {
+    endpoint = `${endpoint}&category_id=${id}`
+  }
+
+  const { data } = await axios.get(endpoint)
+  res.json({
+    next: !!data.next,
+    courses: data.results
+  })
+}
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const { catchErrors } = require('../lib/errorHandlers')
+const controllers = require('../controllers/courses')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('api')
-})
+router.get('/', catchErrors(controllers.getCourses))
 
 module.exports = router
