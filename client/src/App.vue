14551773f3c76534b23034f65d5f3f5ae7f0dd07
Nano <hikarishido1997@gmail.com>
2019-04-01 16:54:45 +0700

    simple footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,15 +5,19 @@
       <v-container>
         <router-view :key="$route.fullPath" />
       </v-container>
+      <app-footer />
     </v-content>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+
 export default {
   components: {
-    'app-header': Header
+    'app-header': Header,
+    'app-footer': Footer
   }
 }
 </script>
---
+++ client/src/components/layout/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer absolute height="auto" style="bottom: -85px">
+    <v-container>
+      <p class="subheading letter-spacing mb-1">
+        Copyright &copy; 2019 Vudemy 🐧. Developed by
+        <a
+          class="blue--text text--lighten-2 decoration-none"
+          href="https://www.facebook.com/Nanoooooooooooooooooooooooooooooooooooooooooooooox"
+          target="_blank"
+        >
+          Nanox
+        </a>
+      </p>
+      <p class="subheading letter-spacing ma-0">
+        Source code is hosted on
+        <a
+          class="blue--text text--lighten-2 decoration-none"
+          href="https://github.com/Nagai-Nano/vudemy"
+          target="_blank"
+        >
+          Github
+        </a>
+      </p>
+    </v-container>
+  </v-footer>
+</template>
